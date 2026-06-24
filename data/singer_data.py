1658fb56898ca09bbaf239ba3d5167b5a5de62c4
Sarthak Singh <114976722+SarthakTools@users.noreply.github.com>
2026-06-24 18:59:08 +0530

    Update singer_data.py

--- data/singer_data.py
+++ data/singer_data.py
@@ -1,1 +1,13 @@
-﻿# New Python file
+artists_data = [
+            {"name": "Arijit Singh",         "file": "arijit_singh.png"},
+            {"name": "G. Gayathri Devi",     "file": "g_gayathri_devi.png"},
+            {"name": "Krishnakumar Kunnath", "file": "kk.png"},
+            {"name": "Alisha Chinai",        "file": "alisha_chinai.png"},
+            {"name": "Sachet Tandon",        "file": "sachet_tandon.png"},
+            {"name": "Parampara Thakur",     "file": "pamphara_thakur.png"},
+            {"name": "Swati Mishra",         "file": "swati_mishra.png"},
+            {"name": "Alan Walker",          "file": "alan_walker.png"},
+            {"name": "Alec Benjamin",        "file": "alec_benjamin.png"},
+            {"name": "Duncan Laurence",      "file": "duncan_laurence.png"},
+            {"name": "Kina Cosper",          "file": "kina.png"},
+        ]
