c98cbd38383cb79feb292ce0fad58240bcaa3ba2
Sarthak Singh <114976722+SarthakTools@users.noreply.github.com>
2026-06-26 21:15:46 +0530

    Update singer_data.py

--- data/singer_data.py
+++ data/singer_data.py
@@ -38,3 +38,26 @@ singer_data = {
             "Somewhere Only We Know" : "Gustixa, Rhianne",
             "Vande Mataram" : "U.V.Niranjan"
 }
+
+
+music_details = [
+    {"name": "Let Me Down Slowly", "image": "let_me_down_slowly.png", "tab": "home1"},
+    {"name": "Raghupathi Raghava Rajaram", "image": "raghupathi_raghava_rajaram.png", "tab": "home1"},
+    {"name": "Somewhere Only We Know", "image": "somewhere_only_we_know.png", "tab": "home1"},
+    {"name": "Vande Mataram Instrumental", "image": "vande_mataram_instrumental.png", "tab": "home1"},
+
+    {"name": "Faded", "image": "faded.png", "tab": "home2"},
+    {"name": "See You Again", "image": "see_you_again.png", "tab": "home2"},
+    {"name": "Hymn For The Weekend", "image": "hymn_for_the_weekend.png", "tab": "home2"},
+    {"name": "Into Your Arms", "image": "into_your_arms.png", "tab": "home2"},
+
+    {"name": "Infinity", "image": "infinity.png", "tab": "home3"},
+    {"name": "Can We Kiss Forever", "image": "can_we_kiss_forever.png", "tab": "home3"},
+    {"name": "Arcade", "image": "arcade.png", "tab": "home3"},
+    {"name": "Safari", "image": "safari.png", "tab": "home3"},
+
+    {"name": "Fearless", "image": "fearless.png", "tab": "home4"},
+    {"name": "Tum Hi Ho", "image": "tum_hi_ho.png", "tab": "home4"},
+    {"name": "Khairiyat", "image": "Khairiyat.png", "tab": "home4"},
+    {"name": "Baaton Ko Teri", "image": "baaton_ko_teri.png", "tab": "home4"},
+]
