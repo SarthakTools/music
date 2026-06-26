b1b65ed8ff706d6f1cb47e2cef9d209d878f9bce
Sarthak Singh <114976722+SarthakTools@users.noreply.github.com>
2026-06-26 20:37:15 +0530

    Create music_data.py

---
+++ data/music_data.py
@@ -0,0 +1,26 @@
+music_details = [
+    {"name": "Let Me Down Slowly", "image": "let_me_down_slowly.png", "tab": "home1"},
+    {"name": "Raghupathi Raghava Rajaram", "image": "raghupathi_raghava_rajaram.png", "tab": "home1"},
+    {"name": "Testing", "image": "testing.png", "tab": "home1"},
+    {"name": "Tu Jo Mila", "image": "tu_jo_mila.png", "tab": "home1"},
+
+    {"name": "Faded", "image": "faded.png", "tab": "home2"},
+    {"name": "See You Again", "image": "see_you_again.png", "tab": "home2"},
+    {"name": "Hymn For The Weekend", "image": "hymn_for_the_weekend.png", "tab": "home2"},
+    {"name": "Into Your Arms", "image": "into_your_arms.png", "tab": "home2"},
+
+    {"name": "Infinity", "image": "infinity.png", "tab": "home3"},
+    {"name": "Can We Kiss Forever", "image": "can_we_kiss_forever.png", "tab": "home3"},
+    {"name": "Arcade", "image": "arcade.png", "tab": "home3"},
+    {"name": "Safari", "image": "safari.png", "tab": "home3"},
+
+    {"name": "Fearless", "image": "fearless.png", "tab": "home4"},
+    {"name": "Tum Hi Ho", "image": "tum_hi_ho.png", "tab": "home4"},
+    {"name": "Khairiyat", "image": "Khairiyat.png", "tab": "home4"},
+    {"name": "Baaton Ko Teri", "image": "baaton_ko_teri.png", "tab": "home4"},
+
+    {"name": "Khamoshiyan", "image": "khamoshiyan.png", "tab": "home5"},
+    {"name": "Tere Sang Yaara", "image": "tere_sang_yaara.png", "tab": "home5"},
+    {"name": "Chhod Diya", "image": "chhod_diya.png", "tab": "home5"},
+    {"name": "Phir Mohabbat", "image": "phir_mohabbat.png", "tab": "home5"},
+]
